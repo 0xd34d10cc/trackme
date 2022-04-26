1a968fe9510e9a06c6bf2d87743cbda302d4ae12
0xd34d10cc <0xd34d10cc@gmail.com>
2022-04-26 09:07:49 +0000

    Remove unused matchers

--- src/matcher.cpp
+++ src/matcher.cpp
@@ -30,74 +30,15 @@ Stats Stats::parse(const Json& data) {
   return stats;
 }
 
-Stats* StatsGroup::get(std::string_view name) {
-  auto it = m_stats.find(name);
-  if (it == m_stats.end()) {
-    it = m_stats.emplace_hint(it, std::string(name), Stats{});
-  }
-
-  return &it->second;
-}
-
-Json StatsGroup::to_json() const {
-  auto group = Json::object();
-  for (auto& [name, stats] : m_stats) {
-    group[name] = stats.to_json();
-  }
-  return group;
-}
-
-void StatsGroup::clear() {
-  auto it = m_stats.begin();
-  while (it != m_stats.end()) {
-    auto& stats = it->second;
-    if (!stats.has_limit()) {
-      it = m_stats.erase(it);
-    } else {
-      stats.clear();
-      ++it;
-    }
-  }
-}
-
-StatsGroup StatsGroup::parse(const Json& data) {
-  StatsGroup group;
-  for (auto& [name, stats] : data.items()) {
-    group.m_stats[name] = Stats::parse(stats);
-  }
-  return group;
-}
-
 ActivityMatcher::~ActivityMatcher() {}
 
-AnyGroupMatcher::~AnyGroupMatcher() {}
+NoneMatcher::~NoneMatcher() {}
 
-Stats* AnyGroupMatcher::match(std::string_view name) {
-  return m_stats.get(name);
+Stats* NoneMatcher::match(std::string_view name) {
+  return nullptr;
 }
 
-Json AnyGroupMatcher::to_json() const {
-  return {
-    {"type", "any"},
-    {"stats", m_stats.to_json() }
-  };
-}
-
-void AnyGroupMatcher::clear() {
-  m_stats.clear();
-}
-
-std::unique_ptr<AnyGroupMatcher> AnyGroupMatcher::parse(const Json& data) {
-  if (data["type"] != "any") {
-    throw std::runtime_error("Failed to parse AnyGroupMatcher: type must be 'any'");
-  }
-
-  auto matcher = std::make_unique<AnyGroupMatcher>();
-  if (data.contains("stats")) {
-    matcher->m_stats = StatsGroup::parse(data["stats"]);
-  }
-  return matcher;
-}
+void NoneMatcher::clear() {}
 
 ListMatcher::ListMatcher(std::vector<std::unique_ptr<ActivityMatcher>> matchers)
   : m_matchers(std::move(matchers))
@@ -113,14 +54,6 @@ Stats* ListMatcher::match(std::string_view name) {
   }
 
   return nullptr;
-}
-
-Json ListMatcher::to_json() const {
-  std::vector<Json> matchers;
-  for (const auto& matcher : m_matchers) {
-    matchers.emplace_back(matcher->to_json());
-  }
-  return std::move(matchers);
 }
 
 void ListMatcher::clear() {
@@ -163,14 +96,6 @@ Stats* RegexMatcher::match(std::string_view name) {
   return nullptr;
 }
 
-Json RegexMatcher::to_json() const {
-  return {
-    {"type", "regex"},
-    {"re", m_expr},
-    {"stats", m_stats.to_json()}
-  };
-}
-
 void RegexMatcher::clear() {
   m_stats.clear();
 }
@@ -188,68 +113,14 @@ std::unique_ptr<RegexMatcher> RegexMatcher::parse(const Json& data) {
   return matcher;
 }
 
-RegexGroupMatcher::RegexGroupMatcher(std::string re)
-  : m_re(re)
-  , m_expr(std::move(re))
-{
-  if (m_re.mark_count() != 1) {
-    throw std::runtime_error("Expected exactly one group in regex");
-  }
-}
-
-RegexGroupMatcher::~RegexGroupMatcher() {}
-
-Stats* RegexGroupMatcher::match(std::string_view name) {
-  std::cregex_token_iterator it(name.data(), name.data() + name.size(), m_re, 1);
-  std::cregex_token_iterator end;
-
-  if (it == end) {
-    return nullptr;
-  }
-
-  return m_stats.get(std::string_view(it->first, it->second));
-}
-
-Json RegexGroupMatcher::to_json() const {
-  return {
-    {"type", "regex_group"},
-    {"re", m_expr},
-    {"stats", m_stats.to_json()}
-  };
-}
-
-void RegexGroupMatcher::clear() {
-  m_stats.clear();
-}
-
-std::unique_ptr<RegexGroupMatcher> RegexGroupMatcher::parse(const Json& data) {
-  if (data["type"] != "regex_group") {
-    throw std::runtime_error("Failed to parse RegexGroupMatcher: type must be 'regex_group'");
-  }
-
-  auto matcher = std::make_unique<RegexGroupMatcher>(data["re"].get<std::string>());
-  if (data.contains("stats")) {
-    matcher->m_stats = StatsGroup::parse(data["stats"]);
-  }
-  return matcher;
-}
-
 std::unique_ptr<ActivityMatcher> parse_matcher(const Json& data) {
   if (data.is_array()) {
     return ListMatcher::parse(data);
   }
 
   const auto type = data["type"];
-  if (type == "any") {
-    return AnyGroupMatcher::parse(data);
-  }
-
   if (type == "regex") {
     return RegexMatcher::parse(data);
-  }
-
-  if (type == "regex_group") {
-    return RegexGroupMatcher::parse(data);
   }
 
   throw std::runtime_error("Unknown matcher type");
--- src/matcher.hpp
+++ src/matcher.hpp
@@ -25,61 +25,22 @@ struct Stats {
   static Stats parse(const Json& data);
 };
 
-class StatsGroup {
-public:
-  StatsGroup() = default;
-  StatsGroup(const StatsGroup&) = default;
-  StatsGroup(StatsGroup&&) noexcept = default;
-  StatsGroup& operator=(const StatsGroup&) = default;
-  StatsGroup& operator=(StatsGroup&&) noexcept = default;
-  ~StatsGroup() = default;
-
-  Stats* get(std::string_view name);
-  Json to_json() const;
-  void clear();
-
-  static StatsGroup parse(const Json& data);
-
-private:
-  struct StringHasher {
-    using is_transparent = void;
-
-    size_t operator()(const std::string& s) const noexcept {
-      return std::hash<std::string_view>()(std::string_view(s));
-    }
-
-    size_t operator()(std::string_view s) const noexcept {
-      return std::hash<std::string_view>()(s);
-    }
-  };
-
-  using StatsMap = std::unordered_map<std::string, Stats, StringHasher, std::equal_to<>>;
-  StatsMap m_stats;
-};
-
 class ActivityMatcher {
 public:
   // returns nullptr if *name* doesn't match any "known" activities
   virtual Stats* match(std::string_view name) = 0;
-  virtual Json to_json() const = 0;
   virtual void clear() = 0;
   virtual ~ActivityMatcher();
 };
 
-// matches *any* activity
-class AnyGroupMatcher: public ActivityMatcher {
-public:
-  AnyGroupMatcher() = default;
-  ~AnyGroupMatcher() override;
+// doesn't match any activity
+class NoneMatcher : public ActivityMatcher {
+ public:
+  NoneMatcher() = default;
+  ~NoneMatcher() override;
 
   Stats* match(std::string_view name) override;
-  Json to_json() const override;
   void clear() override;
-
-  static std::unique_ptr<AnyGroupMatcher> parse(const Json& data);
-
-private:
-  StatsGroup m_stats;
 };
 
 // goes through a list of matchers and returns first that can find a match
@@ -89,7 +50,6 @@ public:
   ~ListMatcher() override;
 
   Stats* match(std::string_view name) override;
-  Json to_json() const override;
   void clear() override;
 
   static std::unique_ptr<ListMatcher> parse(const Json& data);
@@ -105,7 +65,6 @@ public:
   ~RegexMatcher() override;
 
   Stats* match(std::string_view name) override;
-  Json to_json() const override;
   void clear() override;
 
   static std::unique_ptr<RegexMatcher> parse(const Json& data);
@@ -114,23 +73,6 @@ private:
   std::regex m_re;
   std::string m_expr;
   Stats m_stats;
-};
-
-class RegexGroupMatcher: public ActivityMatcher {
-public:
-  RegexGroupMatcher(std::string re);
-  ~RegexGroupMatcher() override;
-
-  Stats* match(std::string_view name) override;
-  Json to_json() const override;
-  void clear() override;
-
-  static std::unique_ptr<RegexGroupMatcher> parse(const Json& data);
-
-private:
-  std::regex m_re;
-  std::string m_expr;
-  StatsGroup m_stats;
 };
 
 // factory function for above
