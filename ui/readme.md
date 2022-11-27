2278f00f374fde8329abee4607c03a6fc6b6324e
Nikita <pl0q1n@gmail.com>
2022-11-27 15:02:04 +0100

    initial tauri ui setup

---
+++ ui/readme.md
@@ -0,0 +1,2 @@
+yarn add -D @tauri-apps/cli
+yarn tauri dev
---
+++ ui/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { invoke } from '@tauri-apps/api'
+
+invoke('greet', { name: 'World' })
+  // `invoke` returns a Promise
+  .then((response) => console.log(response))
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
