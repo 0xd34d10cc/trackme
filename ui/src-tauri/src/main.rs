e653e6156c229ec7461770e9c58727a9391f8491
0xd34d10cc <0xd34d10cc@gmail.com>
2022-12-24 11:50:45 +0000

    Hide to system tray on window close

--- ui/src-tauri/src/main.rs
+++ ui/src-tauri/src/main.rs
@@ -3,23 +3,57 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{
+    CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
 fn main() {
+    let exit = CustomMenuItem::new("exit".to_string(), "Exit");
+    let show = CustomMenuItem::new("show".to_string(), "Show");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(exit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(show);
+
+    let tray = SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => {
+                match id.as_str() {
+                    "exit" => {
+                        // FIXME: graceful shutdown?
+                        std::process::exit(0);
+                    }
+                    "show" => {
+                        tauri::WindowBuilder::new(
+                            app,
+                            "main",
+                            tauri::WindowUrl::App("index.html".into()),
+                        )
+                        // TODO: get title from config
+                        .title("Trackme UI")
+                        .build()
+                        .unwrap();
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![greet])
-        // NOTE: a way to hide the window to system tray, but it still
-        //       consumes GPU & RAM resources
-        // .on_window_event(|event| match event.event() {
-        //     tauri::WindowEvent::CloseRequested { api, .. } => {
-        //         event.window().hide().unwrap();
-        //         api.prevent_close();
-        //     }
-        //     _ => {}
-        // })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        // NOTE: a way to continue running app after the last window has been closed
+        .run(|_app_handle, event| match event {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        });
 }
