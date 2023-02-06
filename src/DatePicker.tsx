4a70cc1a4d011bcfc8bcf09480cb346ddd5fd0b6
0xd34d10cc <0xd34d10cc@gmail.com>
2023-02-06 21:12:34 +0000

    Remove global variable for active dates

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -8,20 +8,7 @@ import {
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { invoke } from "@tauri-apps/api";
 import { getTime } from "date-fns";
-import React from "react";
-
-async function getActiveDates(): Promise<Set<number>> {
-  const response = await invoke("active_dates") as [number]
-  console.log(response)
-  const dates = new Set<number>()
-  for (const date of response) {
-    dates.add(date)
-  }
-  return Promise.resolve(dates)
-}
-
-// TODO: move to effect
-const activeDates = await getActiveDates();
+import React, { useEffect, useState } from "react";
 
 interface CustomPickerDayProps extends PickersDayProps<Date> {
   dayIsUnavailable: boolean;
@@ -35,19 +22,23 @@ const CustomPickersDay = styled(PickersDay, {
   }),
 })) as React.ComponentType<CustomPickerDayProps>;
 
-const renderAvailableDays = (
-  date: Date,
-  _selectedDates: Array<Date | null>,
-  pickersDayProps: PickersDayProps<Date>
-) => {
-  return (
-    <CustomPickersDay
-      {...pickersDayProps}
-      disableMargin
-      dayIsUnavailable={!activeDates.has(getTime(date))}
-    />
-  );
-};
+
+function today(): number {
+  const millisecondsInDay = 24 * 60 * 60 * 1000
+  const date = new Date().getTime()
+  return date - (date % millisecondsInDay)
+}
+
+async function getActiveDates(): Promise<Set<number>> {
+  const response = await invoke("active_dates") as [number]
+  const dates = new Set<number>()
+  for (const date of response) {
+    dates.add(date)
+  }
+
+  console.log(`${dates.size} days active`)
+  return Promise.resolve(dates)
+}
 
 export default function DatePicker({
   date,
@@ -56,6 +47,32 @@ export default function DatePicker({
   date: Date;
   setDate: React.Dispatch<React.SetStateAction<Date>>;
 }) {
+  const [activeDays, setActiveDays] = useState(null as null | Set<number>)
+
+  useEffect(() => {
+    const loadActiveDays = async () => {
+      const dates = await getActiveDates()
+      setActiveDays(dates)
+    }
+
+    loadActiveDays()
+  }, [today()])
+
+  const renderAvailableDays = (
+    date: Date,
+    _selectedDates: Array<Date | null>,
+    pickersDayProps: PickersDayProps<Date>
+  ) => {
+    const isAvailable = activeDays === null || activeDays.has(getTime(date));
+    return (
+      <CustomPickersDay
+        {...pickersDayProps}
+        disableMargin
+        dayIsUnavailable={!isAvailable}
+      />
+    );
+  };
+
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <BaseDatePicker
